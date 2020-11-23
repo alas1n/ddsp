6f40cbe95b65c2270e0978464ce4fa2a499fb5f6
ali <amirzaei26@gmail.com>
2020-11-23 13:53:18 +0330

    tst

--- webapp/ddspwebapp/src/pages/TestPage.js
+++ webapp/ddspwebapp/src/pages/TestPage.js
@@ -1,26 +1,72 @@
-import React from "react";
-import logo from "./../assests/logo.svg";
-// import "./App.css";
+// import React from "react";
+// import logo from "./../assests/logo.svg";
+// // import "./App.css";
 
-const TestPage = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+// const TestPage = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// };
 
-export default TestPage;
+// export default TestPage;
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [42, 33, 68],
+    };
+  }
+
+  onUpdateItem = (i) => {
+    this.setState((state) => {
+      const list = state.list.map((item, j) => {
+        if (j === i) {
+          return item + 1;
+        } else {
+          return item;
+        }
+      });
+
+      return {
+        list,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.list.map((item, index) => (
+            <li key={item}>
+              The person is {item} years old.
+              <button type="button" onClick={() => this.onUpdateItem(index)}>
+                Make me one year older
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
--- webapp/ddspwebapp/src/test.js
+++ webapp/ddspwebapp/src/test.js
@@ -1,27 +1,35 @@
+import React, { Component } from "react";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      list: [42, 33, 68],
+    };
+  }
 
-cTWINKLE_TWINKLE = {
-    notes: [
-      {pitch: 60, startTime: 0.0, endTime: 0.5},
-      {pitch: 60, startTime: 0.5, endTime: 1.0},
-      {pitch: 120, startTime: 1.0, endTime: 1.5},
-      {pitch: 67, startTime: 1.5, endTime: 2.0},
-      {pitch: 69, startTime: 2.0, endTime: 2.5},
-      {pitch: 69, startTime: 2.5, endTime: 3.0},
-      {pitch: 67, startTime: 3.0, endTime: 4.0},
-      {pitch: 65, startTime: 4.0, endTime: 4.5},
-      {pitch: 65, startTime: 4.5, endTime: 5.0},
-      {pitch: 64, startTime: 5.0, endTime: 5.5},
-      {pitch: 64, startTime: 5.5, endTime: 6.0},
-      {pitch: 62, startTime: 6.0, endTime: 6.5},
-      {pitch: 62, startTime: 6.5, endTime: 7.0},
-      {pitch: 60, startTime: 7.0, endTime: 8.0},  
-    ],
-    totalTime: 8
+  onUpdateItems = () => {
+    this.setState((state) => {
+      return state.list.map((item) => item + 1);
+    });
   };
 
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.list.map((item) => (
+            <li key={item}>The person is {item} years old.</li>
+          ))}
+        </ul>
 
-  player = new mm.Player();
-  player.start(TWINKLE_TWINKLE);
-  player.stop();
+        <button type="button" onClick={this.onUpdateItems}>
+          Make everyone one year older
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
