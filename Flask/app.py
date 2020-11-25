4d61aede5b8c944300b834461b1602f521ca5733
ali <amirzaei26@gmail.com>
2020-11-25 15:46:52 +0330

    aaded new url for timber

--- Flask/app.py
+++ Flask/app.py
@@ -1,38 +1,41 @@
 from flask import Flask, request, send_file
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
-
-# from security import authenticate, identity
-# from user import UserRegister
-
+from pydub import AudioSegment
+from timbre_transfer_mine import soundTimberFunc
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class AudioRecorder(Resource):
     def post(self):
         print("audio File received")
-        with open("audio.wav", "wb") as aud:
-                aud_stream = request.files['file'].read()
-                aud.write(aud_stream)
+        with open("audio.mp3", "wb") as aud:
+            aud_stream = request.files['file'].read()
+            aud.write(aud_stream)
+            sound = AudioSegment.from_mp3("audio.mp3")
+            sound.export("audio.wav", format="wav")
+            os.system("ffmpeg -i audio.wav -ar 16000 -ac 1 audio_16K_mono.wav" + " -y") 
         return "Success"
+
+class TimbreTarnsfer(Resource):
     def get(self):
-        # print("request.data",request.args)
-        return send_file('audio.wav', attachment_filename='audio.wav')
+        print(request.args['num'])
+        print(request.args['model'])
+        model = request.args['model']
+        soundTimberFunc('audio_16K_mono.wav',model)
 
-# class AudioReciver(Resource):
-#     def get(self):
-#         print("request.data",request.args)
-#         return send_file('audio.wav', attachment_filename='audio.wav')
-
-# @app.route('/audioreciver/', methods=['GET',])
-# def audioreciver():
-#     print("request",request.args.get)
-#     return send_file('audio.wav', attachment_filename='audio.wav')
-
+        ResynthesisModel = "Resynthesis_" + model
+        resynthesisFile = "%s.wav" % ResynthesisModel
+        low = ResynthesisModel + "_low"
+        lowresynthesisFile = "%s.wav" % low
+        os.system("ffmpeg -i "+ resynthesisFile + " -ar 44100 -ac 1 " + lowresynthesisFile + " -y") 
+        return send_file(lowresynthesisFile, attachment_filename=lowresynthesisFile)
 
 api.add_resource(AudioRecorder, '/audiorecorder/')
-# api.add_resource(AudioReciver, '/audioreciver/')
+api.add_resource(TimbreTarnsfer, '/timbretransfer/')
 
 
 app.run(port=5000, debug=True)
